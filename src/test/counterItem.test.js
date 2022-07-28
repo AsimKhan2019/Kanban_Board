67b3a51981a8402fcda6eab4b74630304c515dc1
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-07-28 20:43:29 +0500

    Added comment test file

---
+++ src/test/counterItem.test.js
@@ -0,0 +1,42 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ describe('Testing items counter', () => {
+  test('Add 1 item to list', () => {
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></li>'
+    + '</div>';
+
+    const data = {
+      index: 0,
+      description: 'This is just a sample',
+      completed: false,
+    };
+    Operations.addToList(data);
+    const tasks = document.querySelectorAll('#list li');
+    expect(tasks).toHaveLength(1);
+  });
+
+  test('Add 2 items to list', () => {
+    localStorage.clear();
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></li>'
+    + '</div>';
+
+    const data = {
+      index: 0,
+      description: 'This is just a sample',
+      completed: false,
+    };
+    const data1 = {
+      index: 1,
+      description: 'This is just another sample',
+      completed: false,
+    };
+    Operations.addToList(data);
+    Operations.addToList(data1);
+    const tasks = document.querySelectorAll('#list li');
+    expect(tasks).toHaveLength(2);
+  });
+});
