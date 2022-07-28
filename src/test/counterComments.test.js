4bedf841bea880ea38a6b773522f0d8a62eac08b
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-07-29 00:23:49 +0500

    Fixed Linters

--- src/test/counterComments.test.js
+++ src/test/counterComments.test.js
@@ -2,29 +2,29 @@
  * @jest-environment jsdom
  */
 
- import InvolvementService from '../modules/InvolvementService.js';
+import InvolvementService from '../modules/InvolvementService.js';
 
- describe('Testing comments counter', () => {
-   test('Getting count for comments', () => {
-     const data = [
-       {
-         comment: 'Lets get together',
-         username: 'Bob',
-         creation_date: '2022-07-27',
-       },
-       {
-         comment: 'Lets get together',
-         username: 'Bob',
-         creation_date: '2022-07-27',
-       },
-       {
-         comment: 'Lets get together',
-         username: 'Bob',
-         creation_date: '2022-07-27',
-       },
-     ];
-     // const luanches = LaunchUI.renderLaunches();
-     const comments = InvolvementService.getCommentsCount(data);
-     expect(comments).toBe(3);
-   });
- });
+describe('Testing comments counter', () => {
+  test('Getting count for comments', () => {
+    const data = [
+      {
+        comment: 'Lets get together',
+        username: 'Bob',
+        creation_date: '2022-07-27',
+      },
+      {
+        comment: 'Lets get together',
+        username: 'Bob',
+        creation_date: '2022-07-27',
+      },
+      {
+        comment: 'Lets get together',
+        username: 'Bob',
+        creation_date: '2022-07-27',
+      },
+    ];
+    // const luanches = LaunchUI.renderLaunches();
+    const comments = InvolvementService.getCommentsCount(data);
+    expect(comments).toBe(3);
+  });
+});
--- src/test/counterItem.test.js
+++ src/test/counterItem.test.js
@@ -1,50 +1,50 @@
 /**
  * @jest-environment jsdom
  */
- import LaunchService from '../modules/LaunchService.js';
+import LaunchService from '../modules/LaunchService.js';
 
- describe('Testing items counter', () => {
-   test('Getting item count for list', () => {
-     const data = [
-       {
-         id: '1',
-         lsp_name: 'Launch Service Provider 1',
-         mission_type: 'mars',
-         name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
-         pad: 'pad1',
-         location: 'location1',
-         image: 'image1',
-       },
-       {
-         id: '2',
-         lsp_name: 'Launch Service Provider 2',
-         mission_type: 'mars',
-         name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
-         pad: 'pad2',
-         location: 'location2',
-         image: 'image2',
-       },
-       {
-         id: '3',
-         lsp_name: 'Launch Service Provider 1',
-         mission_type: 'mars',
-         name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
-         pad: 'pad1',
-         location: 'location1',
-         image: 'image1',
-       },
-       {
-         id: '4',
-         lsp_name: 'Launch Service Provider 1',
-         mission_type: 'mars',
-         name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
-         pad: 'pad1',
-         location: 'location1',
-         image: 'image1',
-       },
-     ];
-     // const luanches = LaunchUI.renderLaunches();
-     const launches = LaunchService.getLauchCount(data);
-     expect(launches).toBe(4);
-   });
- });
+describe('Testing items counter', () => {
+  test('Getting item count for list', () => {
+    const data = [
+      {
+        id: '1',
+        lsp_name: 'Launch Service Provider 1',
+        mission_type: 'mars',
+        name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
+        pad: 'pad1',
+        location: 'location1',
+        image: 'image1',
+      },
+      {
+        id: '2',
+        lsp_name: 'Launch Service Provider 2',
+        mission_type: 'mars',
+        name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
+        pad: 'pad2',
+        location: 'location2',
+        image: 'image2',
+      },
+      {
+        id: '3',
+        lsp_name: 'Launch Service Provider 1',
+        mission_type: 'mars',
+        name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
+        pad: 'pad1',
+        location: 'location1',
+        image: 'image1',
+      },
+      {
+        id: '4',
+        lsp_name: 'Launch Service Provider 1',
+        mission_type: 'mars',
+        name: 'Falcon 9 Block 5 | KPLO (Korean Pathfinder Lunar Orbiter)',
+        pad: 'pad1',
+        location: 'location1',
+        image: 'image1',
+      },
+    ];
+    // const luanches = LaunchUI.renderLaunches();
+    const launches = LaunchService.getLauchCount(data);
+    expect(launches).toBe(4);
+  });
+});
